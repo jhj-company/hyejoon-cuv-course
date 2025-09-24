63e5d8e9253cf33a8961015c8453b446b04aee9d
abko <abko121115@gmail.com>
2025-09-24 20:09:42 +0900

    init: 초기화

---
+++ src/main/java/org/hyejoon/cuvcourse/CuvcourseApplication.java
@@ -0,0 +1,13 @@
+package org.hyejoon.cuvcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuvcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuvcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hyejoon/cuvcourse/CuvcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyejoon.cuvcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuvcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
