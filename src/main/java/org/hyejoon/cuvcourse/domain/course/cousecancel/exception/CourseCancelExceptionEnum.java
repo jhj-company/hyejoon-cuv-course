1a700a009be473689ef6e4001477557efe4be406
Hyjpark <hyej042@gmail.com>
2025-09-25 20:34:40 +0900

    fix(course): 상태 코드 수정

--- src/main/java/org/hyejoon/cuvcourse/domain/course/cousecancel/exception/CourseCancelExceptionEnum.java
+++ src/main/java/org/hyejoon/cuvcourse/domain/course/cousecancel/exception/CourseCancelExceptionEnum.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CourseCancelExceptionEnum implements BusinessExceptionEnum {
-    COURSE_NOT_FOUND(HttpStatus.BAD_REQUEST, "수강 신청 내역이 존재하지 않습니다.");
+    COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "수강 신청 내역이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
