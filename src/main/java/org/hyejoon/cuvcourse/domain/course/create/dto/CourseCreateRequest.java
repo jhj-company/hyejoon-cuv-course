dff2e2c202026315ef2132e4885dfbc36a605510
seongmin2223 <khyuno9@naver.com>
2025-09-25 15:16:44 +0900

    refactor: total 컬럼 제거 및 count 방식으로 수강 인원 확인 로직 변경

--- src/main/java/org/hyejoon/cuvcourse/domain/course/create/dto/CourseCreateRequest.java
+++ src/main/java/org/hyejoon/cuvcourse/domain/course/create/dto/CourseCreateRequest.java
@@ -1,5 +1,9 @@
 package org.hyejoon.cuvcourse.domain.course.create.dto;
 
-public record CourseCreateRequest(int id) {
+import jakarta.validation.constraints.NotNull;
+
+public record CourseCreateRequest(
+    @NotNull(message = "강의 ID는 필수입니다.") Long lectureId
+) {
 
 }
