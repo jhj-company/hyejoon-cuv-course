5dec04acd32f42485b9607bf7b64ab6f0a837261
donghyeon505 <ehdgus458@gmail.com>
2025-09-25 10:12:12 +0900

    feat(lecture): 강의 목록 조회 기능 구현

---
+++ src/main/java/org/hyejoon/cuvcourse/domain/lecture/controller/LectureViewController.java
@@ -0,0 +1,33 @@
+package org.hyejoon.cuvcourse.domain.lecture.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.hyejoon.cuvcourse.domain.lecture.dto.LectureViewResponse;
+import org.hyejoon.cuvcourse.domain.lecture.service.LectureViewService;
+import org.hyejoon.cuvcourse.global.dto.GlobalResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping("/api/lectures")
+public class LectureViewController {
+
+    private final LectureViewService lectureViewService;
+
+    @GetMapping
+    public GlobalResponse<Page<LectureViewResponse>> getAllLectures(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "id"));
+
+        return GlobalResponse.ok("조회 성공", lectureViewService.findAll(pageable));
+    }
+}
