97c890bee0fac8bdf99df7e112fbf1fe572565d0
김동현 <ehdgus458@Gmail.com>
2025-09-25 10:41:36 +0900

    refactor(lecture): 기본 내장 메서드 사용으로 수정

--- src/main/java/org/hyejoon/cuvcourse/domain/lecture/repository/LectureJpaRepository.java
+++ src/main/java/org/hyejoon/cuvcourse/domain/lecture/repository/LectureJpaRepository.java
@@ -1,10 +1,7 @@
 package org.hyejoon.cuvcourse.domain.lecture.repository;
 
 import org.hyejoon.cuvcourse.domain.lecture.entity.Lecture;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LectureJpaRepository extends JpaRepository<Lecture, Long> {
-    Page<Lecture> findAllLecture(Pageable pageable);
 }
