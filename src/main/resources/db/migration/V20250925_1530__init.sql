4301b19164a6ead365e3554f95edcbfab48d8ca5
younghunkimm <hunifive@gmail.com>
2025-09-25 15:18:50 +0900

    chore(db): `docker-compose.yml` 작성 및 `flyway` 세팅

---
+++ src/main/resources/db/migration/V20250925_1530__init.sql
@@ -0,0 +1,30 @@
+CREATE TABLE students (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL COMMENT '학생 이름',
+    email VARCHAR(255) NOT NULL COMMENT '로그인 이메일',
+    password VARCHAR(255) NOT NULL COMMENT '로그인 패스워드',
+    availableCredits INT UNSIGNED NOT NULL COMMENT '신청 가능한 학점',
+    created_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    UNIQUE KEY uk_students_email (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='학생 테이블';
+
+CREATE TABLE lectures (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    lectureTitle VARCHAR(255) NOT NULL COMMENT '강의명',
+    professorName VARCHAR(255) NOT NULL COMMENT '교수명',
+    credits INT UNSIGNED NOT NULL COMMENT '학점',
+    capacity INT UNSIGNED NOT NULL COMMENT '정원',
+    created_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='강의 테이블';
+
+CREATE TABLE courses (
+    lecture_id BIGINT NOT NULL COMMENT '강의 외래키',
+    student_id BIGINT NOT NULL COMMENT '학생 외래키',
+    created_at datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    PRIMARY KEY (lecture_id, student_id),
+    FOREIGN KEY (lecture_id) REFERENCES lectures(id),
+    FOREIGN KEY (student_id) REFERENCES students(id),
+
+    KEY idx_courses_lecture (lecture_id),
+    KEY idx_courses_student (student_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='수강신청 테이블';
