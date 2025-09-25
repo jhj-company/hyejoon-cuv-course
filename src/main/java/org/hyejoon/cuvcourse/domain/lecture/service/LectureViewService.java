51a3ca4ab000bd1f7981022dd1049cca2b79f2d3
donghyeon505 <ehdgus458@gmail.com>
2025-09-25 11:18:51 +0900

    feat(lecture): 강의 목록 조회 기능 구현

---
+++ src/main/java/org/hyejoon/cuvcourse/domain/lecture/service/LectureViewService.java
@@ -0,0 +1,33 @@
+package org.hyejoon.cuvcourse.domain.lecture.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.hyejoon.cuvcourse.domain.lecture.dto.LectureViewResponse;
+import org.hyejoon.cuvcourse.domain.lecture.entity.Lecture;
+import org.hyejoon.cuvcourse.domain.lecture.repository.LectureJpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class LectureViewService {
+
+    private final LectureJpaRepository lectureJpaRepository;
+
+    @Transactional(readOnly = true)
+    public Page<LectureViewResponse> findAll(Pageable pageable) {
+
+        Page<Lecture> lectures = lectureJpaRepository.findAll(pageable);
+
+        return lectures.map(lecture -> new LectureViewResponse(
+            lecture.getId(),
+            lecture.getLectureTitle(),
+            lecture.getProfessorName(),
+            lecture.getCredits(),
+            lecture.getCapacity(),
+            lecture.getTotal()
+        ));
+    }
+}
