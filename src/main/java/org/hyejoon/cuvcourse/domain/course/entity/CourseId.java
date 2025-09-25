3a8fcd9c131e22eeeed7c767e3b17792100e682e
joon448 <joonjdy448@gmail.com>
2025-09-25 21:30:20 +0900

    feat(course): 수강신청 목록 조회 기능 구현

--- src/main/java/org/hyejoon/cuvcourse/domain/course/entity/CourseId.java
+++ src/main/java/org/hyejoon/cuvcourse/domain/course/entity/CourseId.java
@@ -3,16 +3,16 @@ package org.hyejoon.cuvcourse.domain.course.entity;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.io.Serializable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-
 import org.hyejoon.cuvcourse.domain.lecture.entity.Lecture;
 import org.hyejoon.cuvcourse.domain.student.entity.Student;
 
+@Getter
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
