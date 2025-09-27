088f5214d8934c69dd93d4cc5cc27f0d22d734ea
abko <abko121115@gmail.com>
2025-09-27 02:20:45 +0000

    refactor(course): AbstractCourseRegistService를 lock.service로 이동
    refactor(course): AbstractCourseRegistService를 lock.service로 이동
    
    refactor(course): AbstractCourseRegistService를 lock.service로 이동

--- src/main/java/org/hyejoon/cuvcourse/domain/course/courseregist/service/lock/service/CourseRegistService.java
+++ src/main/java/org/hyejoon/cuvcourse/domain/course/courseregist/service/lock/service/CourseRegistService.java
@@ -1,6 +1,5 @@
 package org.hyejoon.cuvcourse.domain.course.courseregist.service.lock.service;
 
-import org.hyejoon.cuvcourse.domain.course.courseregist.service.AbstractCourseRegistService;
 import org.hyejoon.cuvcourse.domain.course.courseregist.service.CourseCreationService;
 import org.hyejoon.cuvcourse.domain.course.courseregist.service.CourseRegistrationValidator;
 import org.hyejoon.cuvcourse.domain.course.courseregist.service.lock.DistributedLock;
