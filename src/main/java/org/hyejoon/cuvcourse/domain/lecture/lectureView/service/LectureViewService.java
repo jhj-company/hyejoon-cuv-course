0c408da8d88e82286f495d326ccbf9403e5c46c1
donghyeon505 <ehdgus458@gmail.com>
2025-09-29 21:10:23 +0900

    refactor(lecture): 총원 필드 비정규화

--- src/main/java/org/hyejoon/cuvcourse/domain/lecture/lectureView/service/LectureViewService.java
+++ src/main/java/org/hyejoon/cuvcourse/domain/lecture/lectureView/service/LectureViewService.java
@@ -2,6 +2,7 @@ package org.hyejoon.cuvcourse.domain.lecture.lectureView.service;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.hyejoon.cuvcourse.domain.lecture.entity.Lecture;
 import org.hyejoon.cuvcourse.domain.lecture.lectureView.dto.LectureViewResponse;
 import org.hyejoon.cuvcourse.domain.lecture.repository.LectureJpaRepository;
 import org.springframework.data.domain.Page;
@@ -18,6 +19,15 @@ public class LectureViewService {
     @Transactional(readOnly = true)
     public Page<LectureViewResponse> findAll(Pageable pageable) {
 
-        return lectureJpaRepository.findLecturesWithTotal(pageable);
+        Page<Lecture> lectures = lectureJpaRepository.findAll(pageable);
+
+        return lectures.map(lecture -> new LectureViewResponse(
+            lecture.getId(),
+            lecture.getLectureTitle(),
+            lecture.getProfessorName(),
+            lecture.getCredits(),
+            lecture.getTotal(),
+            lecture.getCapacity()
+        ));
     }
 }
