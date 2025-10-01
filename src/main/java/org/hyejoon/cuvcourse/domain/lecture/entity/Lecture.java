85a68e615ac65fcf63b10454cb69e5918b656a3b
joon448 <joonjdy448@gmail.com>
2025-10-01 14:06:08 +0900

    feat(course): 검색 최적화 (FULLTEXT INDEX) 및 캐싱 적용

--- src/main/java/org/hyejoon/cuvcourse/domain/lecture/entity/Lecture.java
+++ src/main/java/org/hyejoon/cuvcourse/domain/lecture/entity/Lecture.java
@@ -1,16 +1,16 @@
 package org.hyejoon.cuvcourse.domain.lecture.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import org.hyejoon.cuvcourse.global.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hyejoon.cuvcourse.global.entity.BaseTimeEntity;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
